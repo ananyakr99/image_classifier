8772c41d27af9d2062b867af8bab8a462967fff1
Ananya K R <64872447+ananyakr99@users.noreply.github.com>
2020-05-21 23:22:15 +0530

    Add files via upload

---
+++ train (1).py
@@ -0,0 +1,171 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+import torchvision.models as models
+from PIL import Image
+import json
+from matplotlib.ticker import FormatStrFormatter
+import pandas as pd
+import argparse
+from collections import OrderedDict
+
+parser = argparse.ArgumentParser()
+parser.add_argument('data_dir',type=str, help='Location of directory with data for image classifier to train and test')
+parser.add_argument('-a','--arch',action='store',type=str, help='Choose among 3 pretrained networks - vgg16, alexnet, and densenet121')
+parser.add_argument('-H','--hidden_units',action='store',type=int, help='Select number of hidden units for 1st layer')
+parser.add_argument('-l','--learning_rate',action='store',type=float, help='Choose a float number as the learning rate for the model')
+parser.add_argument('-e','--epochs',action='store',type=int, help='Choose the number of epochs you want to perform gradient descent')
+parser.add_argument('-s','--save_dir',action='store', type=str, help='Select name of file to save the trained model')
+parser.add_argument('-g','--gpu',action='store_true',help='Use GPU if available')
+
+args = parser.parse_args()
+
+data_dir = 'flowers'
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+test_dir = data_dir + '/test'
+
+if args.GPU == 'GPU':
+    device = 'cuda'
+else:
+    device = 'cpu'
+    
+if data_dir:
+    train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([0.485, 0.456, 0.406], 
+                                                            [0.229, 0.224, 0.225])])
+
+test_transforms = transforms.Compose([transforms.Resize(256),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406], 
+                                                           [0.229, 0.224, 0.225])])
+
+validation_transforms = transforms.Compose([transforms.Resize(256),
+                                            transforms.CenterCrop(224),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize([0.485, 0.456, 0.406], 
+                                                                 [0.229, 0.224, 0.225])])
+train_data = datasets.ImageFolder(train_dir, transform=train_transforms)
+validation_data = datasets.ImageFolder(valid_dir, transform=validation_transforms)
+test_data = datasets.ImageFolder(test_dir ,transform = test_transforms)
+
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+vloader = torch.utils.data.DataLoader(validation_data, batch_size =32,shuffle = True)
+testloader = torch.utils.data.DataLoader(test_data, batch_size = 20, shuffle = True) 
+
+with open('cat_to_name.json', 'r') as f:
+    cat_to_name = json.load(f)
+    
+def nn_setup(structure='vgg16',dropout=0.5, hidden_layer1 = 120,lr = 0.001):
+    
+    
+    if structure == 'vgg16':
+        model = models.vgg16(pretrained=True)        
+    elif structure == 'densenet121':
+        model = models.densenet121(pretrained=True)
+    elif structure == 'alexnet':
+        model = models.alexnet(pretrained = True)
+    else:
+        print("Im sorry but {} is not a valid model.Did you mean vgg16,densenet121,or alexnet?".format(structure))
+    for param in model.parameters():
+        param.requires_grad = False  
+        classifier = nn.Sequential(OrderedDict([
+            ('dropout',nn.Dropout(dropout)),
+            ('inputs', nn.Linear(structures[structure], hidden_layer1)),
+            ('relu1', nn.ReLU()),
+            ('hidden_layer1', nn.Linear(hidden_layer1, 90)),
+            ('relu2',nn.ReLU()),
+            ('hidden_layer2',nn.Linear(90,80)),
+            ('relu3',nn.ReLU()),
+            ('hidden_layer3',nn.Linear(80,102)),
+            ('output', nn.LogSoftmax(dim=1))
+                          ]))
+        
+        
+        model.classifier = classifier
+        criterion = nn.NLLLoss()
+        optimizer = optim.Adam(model.classifier.parameters(), lr )
+        model.cuda()
+        
+        return model , optimizer ,criterion 
+
+epochs = 12
+print_every = 5
+steps = 0
+loss_show=[]
+
+model.to('cuda')
+
+for e in range(epochs):
+    running_loss = 0
+    for ii, (inputs, labels) in enumerate(trainloader):
+        steps += 1
+        
+        inputs,labels = inputs.to('cuda'), labels.to('cuda')
+        
+        optimizer.zero_grad()
+        
+        outputs = model.forward(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        
+        if steps % print_every == 0:
+            model.eval()
+            vlost = 0
+            accuracy=0
+                       
+            for ii, (inputs2,labels2) in enumerate(vloader):
+                optimizer.zero_grad()
+                
+                inputs2, labels2 = inputs2.to('cuda:0') , labels2.to('cuda:0')
+                model.to('cuda:0')
+                with torch.no_grad():    
+                    outputs = model.forward(inputs2)
+                    vlost = criterion(outputs,labels2)
+                    ps = torch.exp(outputs).data
+                    equality = (labels2.data == ps.max(1)[1])
+                    accuracy += equality.type_as(torch.FloatTensor()).mean()
+                    
+            vlost = vlost / len(vloader)
+            accuracy = accuracy /len(vloader)                  
+            print("Epoch: {}/{}... ".format(e+1, epochs),
+                  "Loss: {:.4f}".format(running_loss/print_every),
+                  "Validation Lost {:.4f}".format(vlost),
+                   "Accuracy: {:.4f}".format(accuracy))          
+            running_loss = 0
+            
+def check_accuracy_on_test(testloader):    
+    correct = 0
+    total = 0
+    model.to('cuda:0')
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            images, labels = images.to('cuda'), labels.to('cuda')
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
+
+check_accuracy_on_test(testloader)
+
+model.class_to_idx = train_data.class_to_idx
+model.cpu
+torch.save({'structure' :'densenet121',
+            'hidden_layer1':120,
+            'state_dict':model.state_dict(),
+            'class_to_idx':model.class_to_idx},
+            'checkpoint.pth')
